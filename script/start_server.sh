f849668dbe4390ab6cdd4c99287db8406c169aac
Brig Lamoreaux <briglx@users.noreply.github.com>
2024-07-09 15:53:20 -0700

    add check for path

--- script/start_server.sh
+++ script/start_server.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # Variables
-ARTIFACT_ROOT="./artifact/root"
+PROJ_ROOT_PATH=$(cd "$(dirname "$0")"/..; pwd)
+ARTIFACT_ROOT="${PROJ_ROOT_PATH}/artifact/root"
 HOST="0.0.0.0"
 PORT=5000
 LOGGING_CONF="mlflow_logging.conf"
 
-# Create artifact root directory if it does not exist
-mkdir -p $ARTIFACT_ROOT
+# Check if ARTIFACT_ROOT path exists
+if [ -d "$ARTIFACT_ROOT" ]; then
+    echo "The ARTIFACT_ROOT path exists. $ARTIFACT_ROOT"
+else
+    echo "The ARTIFACT_ROOT path does not exist. $ARTIFACT_ROOT"
+fi
 
 # Start MLflow server with logging configuration
 mlflow server \
-    --default-artifact-root $ARTIFACT_ROOT \
-    --host $HOST --port $PORT \
+    --default-artifact-root "$ARTIFACT_ROOT" \
+    --host "$HOST" --port "$PORT" \
     --gunicorn-opts "--log-config $LOGGING_CONF"
