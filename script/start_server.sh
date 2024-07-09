0eb9096c522b2676e9467d263d42e767e1f18ced
Brig Lamoreaux <briglx@users.noreply.github.com>
2024-07-09 15:57:30 -0700

    add check for venv

--- script/start_server.sh
+++ script/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-PROJ_ROOT_PATH=$(cd "$(dirname "$0")"/..; pwd)
+PROJ_ROOT_PATH=$(git rev-parse --show-toplevel)
 ARTIFACT_ROOT="${PROJ_ROOT_PATH}/artifact/root"
 HOST="0.0.0.0"
 PORT=5000
@@ -12,6 +12,26 @@ if [ -d "$ARTIFACT_ROOT" ]; then
     echo "The ARTIFACT_ROOT path exists. $ARTIFACT_ROOT"
 else
     echo "The ARTIFACT_ROOT path does not exist. $ARTIFACT_ROOT"
+fi
+
+# pyenv, pyenv-virtualenv
+if [ -s .python-version ]; then
+    PYENV_VERSION=$(head -n 1 .python-version)
+    export PYENV_VERSION
+fi
+
+if [ -f "${PROJ_ROOT_PATH}/venv/bin/activate" ]; then
+  activate_path="${PROJ_ROOT_PATH}/venv/bin/activate"
+  echo "Found environment in venv $activate_path"
+  # shellcheck disable=SC1090,SC1091
+  . "$activate_path"
+fi
+
+if [ -f "${PROJ_ROOT_PATH}/.venv/bin/activate" ]; then
+  activate_path="${PROJ_ROOT_PATH}/.venv/bin/activate"
+  echo "Found environment in .venv $activate_path"
+  # shellcheck disable=SC1090,SC1091
+  . "$activate_path"
 fi
 
 # Start MLflow server with logging configuration
